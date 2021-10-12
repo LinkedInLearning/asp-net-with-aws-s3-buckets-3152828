b186d1c4754380c7d2b3326611654f219103028d
Ervis Trupja <ervistrupja@outlook.com>
2021-10-12 15:43:23 +0200

    added upload controller

---
+++ AWS.Web/Controllers/UploadController.cs
@@ -0,0 +1,35 @@
+﻿using Amazon;
+using Amazon.S3;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AWS.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private readonly IAmazonS3 _client;
+        public UploadController(IConfiguration config)
+        {
+            _config = config;
+
+            var awsAccess = _config.GetValue<string>("AWSSDK:AccessKey");
+            var awsSecret = _config.GetValue<string>("AWSSDK:SecretKey");
+
+            _client = new AmazonS3Client(awsAccess, awsSecret, RegionEndpoint.EUCentral1);
+        }
+
+        [HttpPost("upload-file/{bucketName}")]
+        public async Task<IActionResult> UploadFile(string bucketName)
+        {
+            return Ok();
+        }
+    }
+}
